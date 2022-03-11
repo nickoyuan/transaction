069ff4f94f7d129859f1540b3ac646ae759451e2
nickyuan <nickyuanfan@gmail.com>
2022-03-11 22:50:14 +1100

    Adding hilt in gradle and setup for viewbinding + databinding

--- app/src/main/java/com/cba/transactionaccount/ui/TransactionAccountFragment.kt
+++ app/src/main/java/com/cba/transactionaccount/ui/TransactionAccountFragment.kt
@@ -1,4 +1,28 @@
 package com.cba.transactionaccount.ui
 
-class TransactionAccountFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.cba.transactionaccount.databinding.TransactionListFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class TransactionAccountFragment : Fragment() {
+
+    private lateinit var binding: TransactionListFragmentBinding
+    private val stakePoolViewModel: TransactionAccountViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = TransactionListFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
 }
