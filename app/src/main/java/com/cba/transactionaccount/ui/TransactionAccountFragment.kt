dc706af7d29aaaabb84a0a34224f80d3e006ba81
nickyuan <nickyuanfan@gmail.com>
2022-03-14 16:34:28 +1100

    Fixing navgraph to a new screen

--- app/src/main/java/com/cba/transactionaccount/ui/TransactionAccountFragment.kt
+++ app/src/main/java/com/cba/transactionaccount/ui/TransactionAccountFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -81,7 +82,7 @@ class TransactionAccountFragment : Fragment() {
     }
 
     private fun setUpTransactionAccountAdapter(transactionAccountRecyclerView: RecyclerView) {
-        transactionAccountAdapter = TransactionAccountCustomAdapter(::transactionHisotryItemClickListener)
+        transactionAccountAdapter = TransactionAccountCustomAdapter(::transactionHistoryItemClickListener)
         with(transactionAccountRecyclerView) {
             adapter = transactionAccountAdapter
             layoutManager = LinearLayoutManager(context)
@@ -89,7 +90,11 @@ class TransactionAccountFragment : Fragment() {
         }
     }
 
-    private fun transactionHisotryItemClickListener(transactionHistory: TransactionHistory) {
-        //findNavController().navigate()
+    private fun transactionHistoryItemClickListener(transactionHistory: TransactionHistory) {
+        val directions =
+            TransactionAccountFragmentDirections.actionTransactionAccountFragmentToTransactionAccountInfo(
+                transactionHistory
+            )
+        findNavController().navigate(directions)
     }
 }
