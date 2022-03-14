53fe74897128b6ee503acf8319f304ce7d2d3b02
nickyuan <nickyuanfan@gmail.com>
2022-03-14 16:34:28 +1100

    Fixing navgraph to a new screen

--- app/src/main/java/com/cba/transactionaccount/ui/TransactionAccountInfo.kt
+++ app/src/main/java/com/cba/transactionaccount/ui/TransactionAccountInfo.kt
@@ -5,13 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.cba.transactionaccount.databinding.TransactionAccountInfoFragmentBinding
+import com.cba.transactionaccount.util.dateToString
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TransactionAccountInfo : Fragment() {
 
     private lateinit var binding: TransactionAccountInfoFragmentBinding
+    val args: TransactionAccountInfoArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,5 +23,15 @@ class TransactionAccountInfo : Fragment() {
     ): View {
         binding = TransactionAccountInfoFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //binding.accountCategoryIcon.setImageDrawable()
+//        binding.accountCategoryDesc
+        binding.accountAmount.text = args.transactionInfoArgs.amount
+        binding.accountDescription.text = args.transactionInfoArgs.description
+        binding.accountTime.text = args.transactionInfoArgs.effectiveDate.dateToString()
     }
 }
