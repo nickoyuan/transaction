fc682333c5bd0a5275f1d8caa053c6772a3d150d
nickyuan <nickyuanfan@gmail.com>
2022-03-14 16:33:11 +1100

    Making data class serializable

--- app/src/main/java/com/cba/transactionaccount/model/TransactionData.kt
+++ app/src/main/java/com/cba/transactionaccount/model/TransactionData.kt
@@ -1,10 +1,11 @@
 package com.cba.transactionaccount.model
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class TransactionData(
     @SerializedName("account")
     val account : AccountData,
     @SerializedName("transactions")
     val transactions : List<TransactionHistory>
-)
+) : Serializable
